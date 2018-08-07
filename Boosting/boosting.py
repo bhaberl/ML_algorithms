6c0043e6be2ac9566f9ddd0caa68fafa49a4b03f
bhaberl <bhaberl.lab@gmail.com>
2018-08-07 23:14:38 +0200

    reorganized the project directory and generated a .py file version

---
+++ Boosting/boosting.py
@@ -0,0 +1,153 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import sframe                            # see below for install instruction
+import matplotlib.pyplot as plt          # plotting
+import numpy as np                       # dense matrices
+from scipy.sparse import csr_matrix      # sparse matrices
+get_ipython().magic('matplotlib inline')
+
+
+# In[ ]:
+
+
+#Load the Lending Club dataset
+loans = sframe.SFrame('lending-club-data.gl/')
+#Exploring some features
+loans.column_names()
+
+# safe_loans =  1 => safe
+# safe_loans = -1 => risky
+loans['safe_loans'] = loans['bad_loans'].apply(lambda x : +1 if x==0 else -1)
+loans = loans.remove_column('bad_loans')
+
+#Selecting features
+target = 'safe_loans'
+features = ['grade',                     # grade of the loan (categorical)
+            'sub_grade_num',             # sub-grade of the loan as a number from 0 to 1
+            'short_emp',                 # one year or less of employment
+            'emp_length_num',            # number of years of employment
+            'home_ownership',            # home_ownership status: own, mortgage or rent
+            'dti',                       # debt to income ratio
+            'purpose',                   # the purpose of the loan
+            'payment_inc_ratio',         # ratio of the monthly payment to income
+            'delinq_2yrs',               # number of delinquincies
+             'delinq_2yrs_zero',          # no delinquincies in last 2 years
+            'inq_last_6mths',            # number of creditor inquiries in last 6 months
+            'last_delinq_none',          # has borrower had a delinquincy
+            'last_major_derog_none',     # has borrower had 90 day or worse rating
+            'open_acc',                  # number of open credit accounts
+            'pub_rec',                   # number of derogatory public records
+            'pub_rec_zero',              # no derogatory public records
+            'revol_util',                # percent of available credit being used
+            'total_rec_late_fee',        # total late fees received to day
+            'int_rate',                  # interest rate of the loan
+            'total_rec_int',             # interest received to date
+            'annual_inc',                # annual income of borrower
+            'funded_amnt',               # amount committed to the loan
+            'funded_amnt_inv',           # amount committed by investors for the loan
+            'installment',               # monthly payment owed by the borrower
+           ]
+# Skipping observations with missing values
+loans, loans_with_na = loans[[target] + features].dropna_split()
+
+# Count the number of rows with missing data
+num_rows_with_na = loans_with_na.num_rows()
+num_rows = loans.num_rows()
+print 'Dropping %s observations; keeping %s ' % (num_rows_with_na, num_rows)
+
+
+
+# In[ ]:
+
+
+#Make sure the classes are balanced
+safe_loans_raw = loans[loans[target] == 1]
+risky_loans_raw = loans[loans[target] == -1]
+
+# Undersample the safe loans.
+percentage = len(risky_loans_raw)/float(len(safe_loans_raw))
+safe_loans = safe_loans_raw.sample(percentage, seed = 1)
+risky_loans = risky_loans_raw
+loans_data = risky_loans.append(safe_loans)
+
+print "Percentage of safe loans                 :", len(safe_loans) / float(len(loans_data))
+print "Percentage of risky loans                :", len(risky_loans) / float(len(loans_data))
+print "Total number of loans in our new dataset :", len(loans_data)
+
+
+# In[ ]:
+
+
+#One-hot encoding
+loans_data = risky_loans.append(safe_loans)
+
+categorical_variables = []
+for feat_name, feat_type in zip(loans_data.column_names(), loans_data.column_types()):
+    if feat_type == str:
+        categorical_variables.append(feat_name)
+
+for feature in categorical_variables:
+    loans_data_one_hot_encoded = loans_data[feature].apply(lambda x: {x: 1})
+    loans_data_unpacked = loans_data_one_hot_encoded.unpack(column_name_prefix=feature)
+
+    # Change None's to 0's
+    for column in loans_data_unpacked.column_names():
+        loans_data_unpacked[column] = loans_data_unpacked[column].fillna(0)
+
+    loans_data.remove_column(feature)
+    loans_data.add_columns(loans_data_unpacked)
+
+loans_data.column_names()
+
+#Split data
+train_data, validation_data = loans_data.random_split(.8, seed=1)
+
+
+# In[ ]:
+
+
+#Making predictions
+validation_safe_loans = validation_data[validation_data[target] == 1]
+validation_risky_loans = validation_data[validation_data[target] == -1]
+
+sample_validation_data_risky = validation_risky_loans[0:2]
+sample_validation_data_safe = validation_safe_loans[0:2]
+
+sample_validation_data = sample_validation_data_safe.append(sample_validation_data_risky)
+sample_validation_data
+
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+def make_figure(dim, title, xlabel, ylabel, legend):
+    plt.rcParams['figure.figsize'] = dim
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if legend is not None:
+        plt.legend(loc=legend, prop={'size':15})
+    plt.rcParams.update({'font.size': 16})
+    plt.tight_layout()
+    
+training_errors = [train_err_10, train_err_50, train_err_100, train_err_200, train_err_500]
+validation_errors = [validation_err_10, validation_err_50, validation_err_100, validation_err_200, validation_err_500]
+
+
+# In[ ]:
+
+
+plt.plot([10, 50, 100, 200, 500], training_errors, linewidth=4.0, label='Training error')
+plt.plot([10, 50, 100, 200, 500], validation_errors, linewidth=4.0, label='Validation error')
+
+make_figure(dim=(10,5), title='Error vs number of trees',
+            xlabel='Number of trees',
+            ylabel='Classification error',
+            legend='best')
+
